ed498a9b874af9626c7d2ea058fd533da11d889d
Paul Castaings <castaings.paul@gmail.com>
2022-11-05 20:24:48 +0100

    solution quest express02

---
+++ app.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+const port = process.env.APP_PORT ?? 5000;
+
+const welcome = (req, res) => {
+  res.send("Welcome to user list");
+};
+
+app.get("/", welcome);
+
+const userHandlers = require("./usersHandlers");
+
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
